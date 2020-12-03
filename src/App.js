decb538fc516bfd5c039c1f4003455a893aac9ad
Nizar Nadif <56386611+NizarNadif@users.noreply.github.com>
2020-12-03 17:32:41 +0100

    modified styles

--- src/App.js
+++ src/App.js
@@ -1,44 +1,56 @@
 import React, {useEffect} from "react";
 import "./style.css";
 
-export function App() {
+export function Calcolatrice() {
+  const [espressione, setEspressione] = React.useState(0);
+
   return (
-    <div className="app">
-      <div className="background">
-        CASIO
-        <pre>fx-5B1BB0F PLUS</pre>
-        <Schermo testo={0} />
-        <Pulsante testo={7} />
-        <Pulsante testo={8} />
-        <Pulsante testo={9} />
-        <Pulsante testo={"DEL"} />
-        <Pulsante testo={"AC"} />
-        <Pulsante testo={4} />
-        <Pulsante testo={5} />
-        <Pulsante testo={6} />
-        <Pulsante testo={"X"} />
-        <Pulsante testo={"÷"} />
-        <Pulsante testo={1} />
-        <Pulsante testo={2} />
-        <Pulsante testo={3} />
-        <Pulsante testo={"+"} />
-        <Pulsante testo={"-"} />
-        <Pulsante testo={0} />
-        <Pulsante testo={"."} />
-        <Pulsante testo={"KEK"} />
-        <Pulsante testo={"Ans"} />
-        <Pulsante testo={"="} />
-      </div>
+    <div className="calcolatrice">
+        <div className="logo">CASIO</div>
+        <pre style={{display: "inline-block"}}>fx-5B1BB0F PLUS</pre>
+      <div style={{fontFamily: "hypocrisy", fontStyle: "italic", marginBottom: "-10px"}}>NATURAL-V.P.A.M.</div>
+      <pre style={{textAlign: "right", marginRight: "15px" }}>1st edition</pre>
+        <Schermo />
+        <Tastiera />
     </div>
   );
 }
 
-export function Schermo(props) {
+export function Schermo(props) { 
   return (
     <div className="schermo">
-      <div>{props.testo}</div>
+      <div style={{display: "inline-block"}}>{'0123456789'}</div>
+      <div className="cursore">|</div>
     </div>
   );
+}
+
+export function Tastiera() {
+  return (
+    <div className="tastiera">
+      <Pulsante testo={7} />
+      <Pulsante testo={8} />
+      <Pulsante testo={9} />
+      <Pulsante testo={"DEL"} />
+      <Pulsante testo={"AC"} />
+      <Pulsante testo={4} />
+      <Pulsante testo={5} />
+      <Pulsante testo={6} />
+      <Pulsante testo={"X"} />
+      <Pulsante testo={"÷"} />
+      <Pulsante testo={1} />
+      <Pulsante testo={2} />
+      <Pulsante testo={3} />
+      <Pulsante testo={"+"} />
+      <Pulsante testo={"-"} />
+      <Pulsante testo={0} />
+      <Pulsante testo={"."} />
+      <Pulsante testo={"KEK"} />
+      <Pulsante testo={"Ans"} />
+      <Pulsante testo={"="} />
+    </div>
+  );
+
 }
 
 export function Pulsante(props) {
@@ -56,11 +68,8 @@ export function Pulsante(props) {
   });
 
   return (
-    <button
-      className="pulsante"
-      onClick={() => { azione(); }}
-    >
+    <a className="pulsante" onClick={() => { azione() }} >
       {props.testo}
-    </button>
+    </a>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,36 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Pulsante, Schermo } from "./App";
+import { Calcolatrice } from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <div className="background">
-      CASIO
-      <pre>fx-5B1BB0F PLUS</pre>
-      <Schermo testo="0" />
-      <Pulsante testo={7} />
-      <Pulsante testo={8} />
-      <Pulsante testo={9} />
-      <Pulsante testo={"DEL"} />
-      <Pulsante testo={"AC"} />
-      <Pulsante testo={4} />
-      <Pulsante testo={5} />
-      <Pulsante testo={6} />
-      <Pulsante testo={"X"} />
-      <Pulsante testo={"÷"} />
-      <Pulsante testo={1} />
-      <Pulsante testo={2} />
-      <Pulsante testo={3} />
-      <Pulsante testo={"+"} />
-      <Pulsante testo={"-"} />
-      <Pulsante testo={0} />
-      <Pulsante testo={"."} />
-      <Pulsante testo={"KEK"} />
-      <Pulsante testo={"Ans"} />
-      <Pulsante testo={"="} />
-    </div>
+    <Calcolatrice />
   </React.StrictMode>,
   rootElement
 );
